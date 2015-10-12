0da66a9804d3442233b34e1e258d6d10bf7425ae
Carl Tanner <carl@wdwhub.net>
2015-10-11 23:07:14 -0700

    ADDED: basic pin drop from geojson

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -1,6 +1,31 @@
 class ExploreController < ApplicationController
   def index
     @eateries = Notebook.new.entries.first(10)
+    @reviews  = FoursquareReview.take(10)
+    @geojson = Array.new
+
+    @reviews.each do |eatery|
+      @geojson << {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [eatery.lng, eatery.lat]
+        },
+        properties: {
+          name: eatery.name,
+          # address: eatery.street,
+          :'marker-color' => '#00607d',
+          :'marker-symbol' => 'circle',
+          :'marker-size' => 'medium'
+        }
+      }
+    end
+    
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @geojson }
+    end
+    
   end
   
   def show
