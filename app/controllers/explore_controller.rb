fcd00e3c599dfe3da4590af310c5be987581c0a8
Carl Tanner <carl@wdwhub.net>
2015-10-15 19:31:34 -0700

    ADDED: details to explore modal

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -29,8 +29,30 @@ class ExploreController < ApplicationController
   end
   
   def show
-    @eateries
-    # @eatery     = @eateries.entries.find(id)
-    @eatery       = Eatery.first
+    @eatery = Notebook.new.entries.find(params[:id])
+    
+    @photos     = @eatery.photos
+    if @photos.length < 10
+      @photos = @photos + @eatery.district.photos
+      if @photos.length < 10
+      	@e = @eatery.district.eateries
+      	@e.each {|e| @photos << e.photos}
+      	@photos = @photos.flatten.to_a.first(10)
+      end      
+    end
+    @photos = @photos.to_a
+    @head_photo = @photos.last
+    @photos     = @photos.first(5)
+    # # @video = Eatery.find_video("#{ename} disney", 3)
+    # @tweet_search_term = Eatery.find_tweets(ename)
+    # @eatery_dfb_links = @eatery.addendums.where('href IS NOT NULL')
+    @dfb_snapshots = @eatery.snapshots.dfb
+    if @dfb_snapshots.length > 0
+      @eatery_dfb_tips = @dfb_snapshots.first.addendums.where(category: 'tips')
+      @eatery_dfb_affinities = @dfb_snapshots.first.addendums.where(category: 'affinity')
+      @eatery_dfb_bloggings = @dfb_snapshots.first.addendums.where(category: 'blogging')
+      
+    end
+    
   end
 end
---
+++ config/initializers/assets.rb
@@ -0,0 +1,1 @@
+Rails.application.config.assets.precompile += ['welcome.js', 'welcome.css', 'explore.js', 'explore.css', 'bootstrap-sprockets.js', 'bootstrap-sprockets.css', 'font-awesome.css']
